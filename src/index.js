9950ea5ccecb886b68bcb22aa7740b056787e38f
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2022-03-10 17:50:39 -0600

    Random Image Start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const unplashRandom = 'https://source.unsplash.com/random';
 
-console.log('Happy hacking :)')
+const mainContainer = document.getElementById('main-container');
+// mainContainer.className = 'flex flex-col justify-center content-center'
+
+const addImgButton = document.createElement('button');
+addImgButton.className = 'w-1/5 rounded-md bg-red-400';
+addImgButton.textContent = 'New Image';
+
+mainContainer.append(addImgButton);
+
+const addNewImage = () => {
+    const imgContainer = document.createElement('div');
+    imgContainer.className = 'w-3/5 p-4 contents';
+    
+    const randomImg = document.createElement('img');
+    randomImg.className = 'rounded-lg';
+    randomImg.src = unplashRandom;
+
+    imgContainer.append(randomImg);
+
+    mainContainer.append(imgContainer);
+
+    console.log(imgContainer);
+}
+
+addImgButton.onclick = addNewImage;
